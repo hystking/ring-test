b00d5d251b7bfb7929ecafa9c2c26c1df605ffd0
Nakayama Yuhei <nakayama-yuhei@kayac.com>
2017-02-09 22:02:40 +0900

    refactor

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,82 @@
 import RendererStats from "./renderer-stats"
 import TimeSkipper from "./time-skipper"
 
+var reflectionTexture = new THREE.CubeTextureLoader().load([
+  "./img/cubemap/posx.jpg",
+  "./img/cubemap/negx.jpg",
+  "./img/cubemap/posy.jpg",
+  "./img/cubemap/negy.jpg",
+  "./img/cubemap/posz.jpg",
+  "./img/cubemap/negz.jpg",
+]);
+
+/*
+const reflectionTexture = new THREE.TextureLoader().load("./img/equirecmap.jpg");
+reflectionTexture.mapping = THREE.EquirectangularReflectionMapping;
+reflectionTexture.magFilter = THREE.LinearFilter;
+reflectionTexture.minFilter = THREE.LinearMipMapLinearFilter;
+*/
+
+var hairline = new THREE.TextureLoader().load("./img/hairline.png");
+hairline.repeat.set(8, 2);
+hairline.wrapS = hairline.wrapT = THREE.RepeatWrapping;
+
 function pingPong(t) {
   t = t % (Math.PI * 2)
   if(t < Math.PI) {
     return t / Math.PI
   }
   return 1 - (t - Math.PI) / Math.PI
+}
+
+function generateRingGeometry(radius) {
+  const points = [];
+  const segments = 127
+  for(let i=0; i<segments; i++) {
+    const theta = Math.PI * 2 * i / segments;
+    const wave = theta < Math.PI ? 1 : Math.pow(pingPong((theta - Math.PI) * 4), 2) * .4 + 1
+    let x = Math.sin(theta) * (2 - Math.sin(theta)) * wave * .2;
+    let y = Math.cos(theta) * 1;
+    if(x < -.555) {
+      x = (x + -.555 * 7) / 8
+    }
+    if(x > .15) {
+      x = (x + .15 * 7) / 8
+    }
+    x -= radius;
+    points.push(new THREE.Vector2(x, y));
+  }
+  points.push(points[0].clone());
+  return new THREE.LatheBufferGeometry(points, 128);
+}
+
+function generateRingMesh(radius) {
+  var material = new THREE.MeshStandardMaterial({
+    envMap: reflectionTexture,
+    roughness: .74,
+    metalness: .99,
+    color: 0xf6fffc,
+    bumpMap: hairline,
+    bumpScale: -.001,
+  });
+
+  /*
+  var material = new THREE.MeshStandardMaterial({
+    envMap: reflectionTexture,
+    roughness: .4,
+    metalness: .9,
+    color: 0xf6fffc,
+    bumpMap: hairline,
+    bumpScale: -.001,
+  });
+  */
+
+  const geometry = generateRingGeometry(radius);
+  const mesh = new THREE.Mesh(geometry, material);
+  mesh.rotation.z = Math.PI / 2;
+  mesh.rotation.y = - Math.PI / 6;
+  mesh.position.y = 0;
+  return mesh;
 }
 
 export default function index() {
@@ -35,63 +105,7 @@ export default function index() {
   controls.target.set(0, 0, 0)
   controls.maxPolarAngle = Math.PI * .45
 
-  var reflectionCube = new THREE.CubeTextureLoader().load([
-    "./img/cubemap/posx.jpg",
-    "./img/cubemap/negx.jpg",
-    "./img/cubemap/posy.jpg",
-    "./img/cubemap/negy.jpg",
-    "./img/cubemap/posz.jpg",
-    "./img/cubemap/negz.jpg",
-  ]);
-
-  /*
-  const reflectionEquirec = new THREE.TextureLoader().load("./img/equirecmap.jpg");
-  reflectionEquirec.mapping = THREE.EquirectangularReflectionMapping;
-  reflectionEquirec.magFilter = THREE.LinearFilter;
-  reflectionEquirec.minFilter = THREE.LinearMipMapLinearFilter;
-  */
-
-  var hairline = new THREE.TextureLoader().load("./img/hairline.png");
-  hairline.repeat.set(8, 2);
-  hairline.wrapS = hairline.wrapT = THREE.RepeatWrapping;
-
-  var cubemapMaterial = new THREE.MeshStandardMaterial({
-    envMap: reflectionCube,
-    roughness: .74,
-    metalness: .99,
-    color: 0xf6fffc,
-    bumpMap: hairline,
-    bumpScale: -.001,
-  });
-
-  var equirecmapMaterial = new THREE.MeshStandardMaterial({
-    // envMap: reflectionEquirec,
-    roughness: .4,
-    metalness: .9,
-    color: 0xf6fffc,
-    bumpMap: hairline,
-    bumpScale: -.001,
-  });
-
-  const points = [];
-  const segments = 255
-  for(let i=0; i<segments; i++) {
-    const theta = Math.PI * 2 * i / segments;
-    const wave = theta < Math.PI ? 1 : Math.pow(pingPong((theta - Math.PI) * 4), 2) * .4 + 1
-    points.push(new THREE.Vector2(
-      Math.sin(theta) * (2 - Math.sin(theta)) * wave * .2 - 6,
-      Math.cos(theta) * 1,
-    ));
-  }
-  points.push(points[0].clone());
-  const geometry = new THREE.LatheBufferGeometry(points, 256);
-
-  const mesh = new THREE.Mesh(geometry, cubemapMaterial);
-  // const mesh = new THREE.Mesh(geometry, equirecmapMaterial);
-  mesh.rotation.z = Math.PI / 2;
-  mesh.rotation.y = - Math.PI / 6;
-  mesh.position.y = .3;
-
+  const mesh = generateRingMesh(5)
   scene.add(mesh);
 
   /* Floor */
@@ -101,10 +115,11 @@ export default function index() {
     textureHeight: 2048,
     color: 0xaaaaad,
   });
+
   const floorGeometry = new THREE.PlaneBufferGeometry(100, 100, 4);
   const floor = new THREE.Mesh(floorGeometry, floorMirror.material);
   floor.rotation.set(- Math.PI / 2, 0, 0);
-  floor.position.set(0, -6.3, 0);
+  floor.position.set(0, -5 -.555, 0);
   floor.add(floorMirror);
   scene.add(floor);
 
